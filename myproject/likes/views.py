68f5db506eed6e89cdf9d19601b0e3bc7800da5a
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-18 19:39:11 +0000

    api basics done it

--- myproject/likes/views.py
+++ myproject/likes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from posts.views import PostViewSet
+from comments.views import CommentViewSet
+from profiles.views import ProfileViewSet
+from likes.views import LikeViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'profiles', ProfileViewSet)
+router.register(r'likes', LikeViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
