daeee67b81414d42c2f9e958b66cf55fb8bc1583
giacoren6 <142323106+giacoren6@users.noreply.github.com>
2024-11-18 19:30:08 +0000

    creating apps adding models to the apps

---
+++ myproject/comments/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from posts.models import Post
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ myproject/likes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from posts.models import Post
+from django.contrib.auth.models import User
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
